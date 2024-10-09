fc10108ece3025d2380722457ef3ea3ba5a53084
Milkytaa <qywurwat@gmail.com>
2024-10-09 14:53:36 +0700

    change name

--- transaction_history.php
+++ transaction_history.php
@@ -10,7 +10,7 @@ $expenses = get_expenses();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transaction History - Expense Tracker</title>
+    <title>Transaction History - W-Tracker</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </head>
